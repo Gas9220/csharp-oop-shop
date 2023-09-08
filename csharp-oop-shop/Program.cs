9e583bc225f3aca182fcbc8dfde78096901aa322
Gaspare <Gas9220@users.noreply.github.com>
2023-09-08 14:16:42 +0200

    Basic product class definition

--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿// Product class definition
+public class Product
+{
+    private int code;
+    private string name;
+    private string description;
+    private float price;
+    private int vat;
+
+    public Product(int code, string name, string description, float price, int vat) {
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.vat = vat; 
+    }
+}
