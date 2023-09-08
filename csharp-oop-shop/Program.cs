0a433db7d6df66b8b3a5998e58af8c99991c6294
Gaspare <Gas9220@users.noreply.github.com>
2023-09-08 14:34:52 +0200

    GenerateProductCode() function

--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,17 +1,26 @@
 ﻿// Product class definition
 public class Product
 {
-    private int code;
+    // Attributes
+    private string code;
     private string name;
     private string description;
     private float price;
     private int vat;
 
-    public Product(int code, string name, string description, float price, int vat) {
-        this.code = code;
+    // Constructor
+    public Product(string name, string description, float price, int vat) {
+        code = GenerateProductCode();
         this.name = name;
         this.description = description;
         this.price = price;
         this.vat = vat; 
+    }
+
+    // Methods
+    string GenerateProductCode()
+    {
+        Random rnd = new Random();
+        return this.code = rnd.Next(00000000, 99999999).ToString();
     }
 }
